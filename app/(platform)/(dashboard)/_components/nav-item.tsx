3bafd6fdbedf19e9c12eb244e5a79c37a840bc05
aayush <aausgh@gmail.com>
2024-03-30 11:16:07 +0545

    add server actions

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -9,6 +9,7 @@ import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/
 
 import { Activity, CreditCard, Layout, Settings } from 'lucide-react'
 import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
 
 
 export type Organization = {
@@ -101,5 +102,16 @@ export const NavItem = ({ isExpanded, isActive, organization, onExpand }: NavIte
                 ))}
             </AccordionContent>
         </AccordionItem>
+    )
+}
+
+NavItem.Skeleton = function SkeletonNavItem() {
+    return (
+        <div className="flex items-center gap-x-2">
+            <div className="w-10 h-10 relative shrink-0">
+                <Skeleton className="h-full w-full absolute" />
+            </div>
+            <Skeleton className="h-10 w-full" />
+        </div>
     )
 }
